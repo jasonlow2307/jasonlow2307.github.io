b9f0b0a790bbc17da4ab1bc24b305d637f5f43ba
jasonlow2307 <jason.lowjx@gmail.com>
2022-11-21 12:53:02 +0800

    Update

--- signup_script.js
+++ signup_script.js
@@ -1,9 +1,9 @@
 function validateLogin(){
-    let userName = document.forms["login_form"]["user_name"].value;
-    let password = document.forms["login_form"]["password"].value;
-    let re_password = document.forms["login_form"]["re_password"].value;
+    let userName = document.forms["signup_form"]["user_name"].value;
+    let password = document.forms["signup_form"]["password"].value;
+    let re_password = document.forms["signup_form"]["re_password"].value;
     const welcome_banner = document.getElementById('welcome_banner');
-    const login_form = document.getElementById('login_form');
+    const signup_form = document.getElementById('signup_form');
 
     if (userName == "") {
         alert("Please fill in your user name");
@@ -21,15 +21,11 @@ function validateLogin(){
     } else if(re_password.localeCompare(password) != 0){
         alert("Please enter the same password for the repeating password");
         return false;
-    } 
-
-    if(userName === "admin" && password === "admin" && re_password === "admin") {
-        alert("Welcome, you have logged in succesfully");
-        welcome_banner.textContent = 'Welcome Admin, you have logged in successfully';
+    } else {
+        alert("Welcome, you have signed up succesfully");
+        welcome_banner.textContent = 'Welcome ' + userName + ', you have signed up successfully';
         welcome_banner.style.marginBottom = 150;
-        login_form.style.display = 'none';
+        signup_form.style.display = 'none';
         return false;
-    } else{
-        alert("Wrong login credentials, please try again.");
     }
 }
